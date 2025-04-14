98ed8b69417b4538b29f08d6e72c6a916bbce1cc
Alexandra <alexandralsilva91@gmail.com>
2025-04-14 14:03:53 +0100

    footer, but not completed

--- exercicios/html-css-final/loja/react/src/App.jsx
+++ exercicios/html-css-final/loja/react/src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/header/Header"
 import SplashScreen from "./components/body/SplashScreen"
 import Section from "./components/sections/Section"
-import Title from "./components/sections/Title"
 import SectionTwo from "./components/sections/SectionTwo"
 import SectionThird from "./components/sections/SectionThird"
 import SectionFourth from "./components/sections/SectionFourth"
 import CarousselSectionFive from "./components/sections/CarousselSectionFive"
 import Button from "./components/body/Button"
 import SectionOne from "./components/sections/SectionOne"
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -38,6 +38,7 @@ function App() {
           <Button>Shop seating</Button>
         </div>
       </Section>
+      <Footer />
     </>
   );
 }

b4994759c04e2493980d5a9cacfc8624db776e68
Alexandra <alexandralsilva91@gmail.com>
2025-04-30 15:21:45 +0100

    commit all

--- exercicios/html-css-final/loja/react/src/components/ProductDetails.jsx
+++ exercicios/html-css-final/loja/react/src/components/ProductDetails.jsx
@@ -10,35 +10,38 @@ function ProductDetails({ colectionTitle, productName, imgSource, productPrice, 
                 <img src={imgSource} alt="product" />
             </div>
             <div className="product-details-text">
-                <p className="product-name">{productName}</p>
-                <span>{`$${productPrice} + Free Shipping`}</span>
-                <div>
-                    <div><strong>Fabric Color</strong>-{fabricColor}</div>
-                    <div className="colors">
-                        {colorIcons.map((colorIcon, index) => {
-                            let className = "color-icon"
-                            if( index === 0) {
-                                className = `${className} selected`
-                            } 
-                            return(
-                                <div key={`color-icon-${index}`} className={className} style={{backgroundColor: colorIcon}}/>
-                            )
-                        })}
-
+                    <p className="product-denomination">{productName}</p>
+                    <span>${productPrice} + Free Shipping</span>
+                    <div>
+                        <div><strong>Fabric Color</strong>-{fabricColor}</div>
+                        <div className="colors">
+                            {colorIcons.map((colorIcon, index) => {
+                                let className = "color-icon"
+                                if (index === 0) {
+                                    className = `${className} selected`
+                                }
+                                return (
+                                    <div key={`color-icon-${index}`} className={className} style={{ backgroundColor: colorIcon }} />
+                                )
+                            })}
+                        </div>
                     </div>
+                    <div className="upgrades">
+                        <strong>Upgrades</strong>
+                        <div className="upgrade-options">
+                            {upgradeOptions.map((upgradeOption, index) => (
+                                <div key={`upgrade-option-${index}`} className="upgrade-option">
+                                    <input type="checkbox" id={`upgrade-option-input-${index}`} name="scales" />
+                                    <label for="upgrade-option-label">{`${upgradeOption.name} + $${upgradeOption.price}`}</label>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                
+                <div className="button-container">
+                    <div className="spacer"></div>
+                    <Button className="product-details-btn">Add to cart</Button>
                 </div>
-                <div className="upgrades">
-                    <strong>Upgrades</strong>
-                    <div className="upgrade-options">
-                        {upgradeOptions.map((upgradeOption, index) => (
-                            <div key={`upgrade-option-${index}`} className="upgrade-option">
-                                <input type="checkbox" id={`upgrade-option-input-${index}`} name="scales" />
-                                <label for="upgrade-option-label">{`${upgradeOption.name} + $${upgradeOption.price}`}</label>
-                            </div>
-                        ))}
-                    </div>
-                </div>   
-                <Button className="product-details-btn">Add to cart</Button>
             </div>
         </div>
     )
